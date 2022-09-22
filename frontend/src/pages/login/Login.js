e2cff5a9548c95cd8ea0e6a8e8659344811029d9
SouadTorfi <souad.torfi99@gmail.com>
2022-09-22 22:29:06 +0300

    Added apollo graphql to reactjs and login page

---
+++ frontend/src/pages/login/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Login.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_Users = gql`
+  {
+    users {
+      name
+      email
+    }
+  }
+`;
+function Login() {
+  const { data, error } = useQuery(GET_Users);
+  console.log("data", data);
+  console.log("error", error);
+
+  return (
+    <div>
+      <div className="loginPage">
+        <h2>Login Page</h2>
+        <form className="loginForm">
+          <input type="text" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button type="submit" className="loginbtn">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
